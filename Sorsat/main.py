26fa7a7e708eef802d92487f7f15a9c24c3f8095
viltsu28 <ville.seeste@hotmail.com>
2018-10-18 00:38:12 +0300

    rasa test

--- Sorsat/main.py
+++ Sorsat/main.py
@@ -1,9 +1,16 @@
 import SpeechRecoModule as stt
+import requests
+import json
 
 def printSpeech(speech):
     for say in speech:
-        print say
-
+        #print say
+        url = 'http://192.168.1.105:5005/conversations/default/respond'
+        headers = {"Accept": "application/json", "Accept-Charset": "utf-8"}
+        payload = {'query': say}
+        response = requests.post(url, data=json.dumps(payload))	
+        print response.text
+		
 def main():
 
     # This Uses SpeechRecoModule to yield spoken sentences.
